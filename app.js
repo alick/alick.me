d74dbf7931c99296520ba277afd840a9be8593fc
Alick Zhao <alick@helium>
2014-08-05 00:32:07 +0800

    app.js: use openshift env variables

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
 // REF: http://clock.co.uk/tech-blogs/a-simple-website-in-nodejs-with-express-jade-and-stylus
+
+//  Set the environment variables we need.
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP;
+var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+
+if (typeof ipaddress === "undefined") {
+  //  Log errors on OpenShift but continue w/ 127.0.0.1 - this
+  //  allows us to run/test the app locally.
+  console.warn('No OPENSHIFT_NODEJS_IP var, using 127.0.0.1');
+  ipaddress = "127.0.0.1";
+};
+
 /*
  * Module dependencies
  */
@@ -99,6 +111,9 @@ app.get('/500', function (req, res, next) {
   next(new Error('Unknown. Pray for peace.'));
 });
 
-app.listen(2460)
+app.listen(port, ipaddress, function() {
+  console.log('%s: Node server started on %s:%d ...',
+    Date(Date.now()), ipaddress, port);
+});
 
 // vim: set sw=2 et:
