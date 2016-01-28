d588e430a6f48f94f4b2ce48965268b02c29be88
Alick Zhao <alick@helium>
2016-01-27 23:06:44 -0600

    finish translation to English

--- locales/zh.js
+++ locales/zh.js
@@ -1,5 +1,5 @@
 {
-  "Homepage": "首页",
+  "the homepage": "首页",
   "About": "关于",
   "Welcome!": "欢迎光临！",
   "Here are links to Alick's footprints on the Internet.": "这里链接了 Alick 在网络上发布与分享的各种内容。",
@@ -19,8 +19,19 @@
   "Contact": "联系方式",
   "Domain name": "域名",
   "Changelog": "主要变更",
-  "Internationalization": "支援国际化。",
+  "Internationalization.": "支援国际化。",
   "Use Bootstrap.": "引入 Bootstrap。",
   "3.x to 4.x.": "从 3.x 迁移到 4.x。",
-  "Migrate server to OpenShift PaaS platform (essentially on Amazon AWS).": "网站迁移到 OpenShift PaaS 平台，实质托管在 Amazon AWS。"
+  "Migrate server to OpenShift PaaS platform (essentially on Amazon AWS).": "网站迁移到 OpenShift PaaS 平台，实质托管在 Amazon AWS。",
+  "Email preferred: ": "优先邮箱：",
+  "IRC: alick at freenode": "IRC: alick 于 freenode",
+  "Social Network Sites: Check <a href=\"/en/\">the homepage</a>.": "社交网站：参见<a href=\"/zh/\">首页</a>。",
+  "I'm Tao Zhao from China, currently a Ph.D. student in Texas A&M University.": "我是赵涛，河南许昌人，现为德州 A&M 大学博士研究生。",
+  "I love free and open source technologies and culture.": "爱好自由与开源技术及其背后的开放协作文化。",
+  "I am a <a href='https://fedoraproject.org/wiki/User:Alick'>Fedora Project contributor</a>.": "我是一名 <a href='https://fedoraproject.org/wiki/User:Alick/Zh'>Fedora 社区贡献者</a>。",
+  "This site is built with Node.js, Express, Jade, Stylus and others.": "本站使用 Node.js, Express, Jade, Stylus 等搭建。",
+  "If not specified otherwise, the contents are licensed under <a href='http://creativecommons.org/licenses/by-sa/4.0/deed' title='Attribution-ShareAlike 4.0 International'>CC BY-SA 4.0</a>.": "如无特殊声明，本站内容以 <a href='http://creativecommons.org/licenses/by-sa/4.0/deed.zh' title='署名-相同方式共享 4.0 国际'>CC BY-SA 4.0</a> 许可授权使用。",
+  "Alick is my English name picked up in the dictionary by myself.": "Alick 是我初二时从字典里附录中挑出来的英文名，沿用至今。",
+  "\"me\" is the country code top-level domain for the country <a href='http://en.wikipedia.org/wiki/.me'>Montenegro</a>, but also apparently suitable for personal websites.": "\"me\" 是<a href='http://zh.wikipedia.org/wiki/.me'>黑山</a>这个国家的顶级域名，但同时也是英文中的“我”，所以被广泛用于个人网站。",
+  "Homepage": "首页"
 }
